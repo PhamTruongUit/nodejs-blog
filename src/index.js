bf774de2843ee91e7a335399ef2e56b3b64424f4
PhamTruong <18521571@gm.uit.edu.vn>
2021-09-14 21:09:30 +0700

    layouts template

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const morgan = require('morgan')
 const path = require('path')
 const app = express()
 const port = 3000
 
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine handlebars
 app.engine('handlebars', handlebars())
-app.set('view engine', 'handlebars')
-app.set('view', path.join(__dirname + 'resouces\\views'))
-// console.log(__dirname + 'resouces\\views')
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        // helpers: {
+        //     sum: (a, b) => a + b,
+        // },
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 app.get('/home', (req, res) => res.render('home'))
+app.get('/new', (req, res) => res.render('new'))
 
 app.listen(port, ()=> console.log(`http://localhost:${port}`));
